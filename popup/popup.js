e5537144f4b180f28d2ef645fe1fb35dd25e12f3
ben <benoit.works@gmail.com>
2016-12-13 09:56:26 +0100

    fix settings button

--- popup/popup.js
+++ popup/popup.js
@@ -2,20 +2,23 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
     document.getElementById('time').innerHTML = timeFormater(request.time);
 });
 
-
 document.getElementById('settings').addEventListener('click', function () {
-//todo
+    var currentTime =  localStorage.getItem('time') || 10 ;
+    var inputUserTime = prompt('Enter time :', new String(currentTime));
+    localStorage.setItem('time', inputUserTime);
+    chrome.runtime.sendMessage({settings : true, time : inputUserTime}, function(response) {
+    });
 });
 
 
 document.getElementById('pause').addEventListener('click', function () {
-	 chrome.runtime.sendMessage({pause : true}, function(response) {
-     });
+  chrome.runtime.sendMessage({pause : true}, function(response) {
+  });
 });
 
 document.getElementById('resume').addEventListener('click', function () {
-     chrome.runtime.sendMessage({resume : true}, function(response) {
-     });
+   chrome.runtime.sendMessage({resume : true}, function(response) {
+   });
 });
 
 function timeFormater (timeInSeconds) {
@@ -23,3 +26,4 @@ function timeFormater (timeInSeconds) {
     date.setSeconds(timeInSeconds);
     return date.toISOString().substr(11, 8);
 }
+
